ab095bb1a21c15b7a7e51886f47e17ce4be27b2e
thomashseedorf <tom.seedorf@gmail.com>
2023-01-25 18:43:10 -0600

    Update to file structure
    Updated repo structure
    Added Docker config

---
+++ adventuregame/__init__.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='adventuregame',
+      version='2.0',
+      description='A retro text-based adventure game',
+      url='https://github.com/thomashseedorf/adventureGame',
+      author='Thomas Seedorf',
+      author_email='tom.seedorf@gmail.com',
+      license='MIT',
+      packages=['adventuregame'],
+      zip_safe=False)
