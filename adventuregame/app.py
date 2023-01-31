c051bb6f49b2e92674cb960fba3fc81848ca19de
thomashseedorf <100506549+thomashseedorf@users.noreply.github.com>
2023-01-30 20:38:21 -0600

    consolidated welcome into app

--- adventuregame/app.py
+++ adventuregame/app.py
@@ -15,6 +15,7 @@ import superpie
 import random
 import multiprocessing
 import cv2
+from termcolor import cprint
 
 # CONSTANTS
 FORWARD = "W"
@@ -159,6 +160,10 @@ def cantDo():
 def music(intro):
     playsound('intro.mp3')
 intro = multiprocessing.Process(target=music, args=(1,))
+
+def music(title):
+    playsound('title.mp3')
+title = multiprocessing.Process(target=music, args=(1,))
 
 def music(fadedying):
     playsound('fadedying.mp3')
@@ -393,7 +398,56 @@ def death_animate():
 def main():
     # Game start
     clear()
-    welcome.print_welcome()
+    title.start() # Title music thread start
+    try:
+        col = os.get_terminal_size().columns
+    except:
+        col = 200
+    welcome_str = """
+                                               _____________   ____________  __________   
+                                              / ____/ ____/ | / / ____/ __ \/  _/ ____/   
+                                             / / __/ __/ /  |/ / __/ / /_/ // // /        
+                                            / /_/ / /___/ /|  / /___/ _, _// // /___      
+                                            \____/_____/_/ |_/_____/_/ |_/___/\____/      
+                                                                    
+                          _____________  ________   ___    ____ _    _________   __________  ______  ______   
+                         /_  __/ ____/ |/ /_  __/  /   |  / __ \ |  / / ____/ | / /_  __/ / / / __ \/ ____/   
+                          / / / __/  |   / / /    / /| | / / / / | / / __/ /  |/ / / / / / / / /_/ / __/      
+                         / / / /___ /   | / /    / ___ |/ /_/ /| |/ / /___/ /|  / / / / /_/ / _, _/ /___      
+                        /_/ /_____//_/|_|/_/    /_/  |_/_____/ |___/_____/_/ |_/ /_/  \____/_/ |_/_____/      
+                                                                                                            
+                                                   _________    __  _________
+                                                  / ____/   |  /  |/  / ____/
+                                                 / / __/ /| | / /|_/ / __/   
+                                                / /_/ / ___ |/ /  / / /___   
+                                                \____/_/  |_/_/  /_/_____/    
+                                                
+"""
+    print(welcome_str.center(col))
+    print(slow_type('                                                  Created by Tom Seedorf                '))
+    print()
+    cprint("                                                   Press Enter to start", attrs=['blink'])
+    begin = input('> ')
+    clear()
+    title.terminate()
+    print(slow_type('''
+    
+You see a large castle in the distance...
+
+
+Its beauty entrances you...
+
+
+Is that someone in the tower?
+
+
+You stop and peer upward at a brilliant yellow belfry...
+
+
+You hope to remain unseen while determining your next move...
+
+
+You look for clues to assist your entry..........'''))
     intro.start() # Intro music thread start
     print()
     castlegif.start() #Gif start
